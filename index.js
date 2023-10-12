6289791e8f87e7a11642d2b82bd4376cb73883f8
louay <louaysakli65@gmail.com>
2023-10-12 15:39:13 +0100

    done

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+var imagepr = document.querySelector(".img");
+var colorbg = document.querySelector(".container");
+
+function phones(phone) {
+  imagepr.src = phone;
+}
+function colors(color) {
+  colorbg.style.background = color;
+}
 document.addEventListener("DOMContentLoaded", function () {
   var userStatusDiv = document.getElementById("user-status");
-  var isLoggedIn = localStorage.getItem("isLoggedIn")
-  var username = localStorage.getItem("username")
-var password = localStorage.getItem("password")
-
+  var isLoggedIn = localStorage.getItem("isLoggedIn");
+  var username = localStorage.getItem("username");
+  var password = localStorage.getItem("password");
   if (isLoggedIn === "true") {
     var greetingMessage = document.createElement("p");
     greetingMessage.textContent = `Welcome, ${username}!`;
@@ -12,14 +20,14 @@ var password = localStorage.getItem("password")
     logoutButton.addEventListener("click", function () {
       localStorage.removeItem("isLoggedIn");
       localStorage.removeItem("username");
-        localStorage.removeItem("password")
-      window.location.href = "index.html";
+      localStorage.removeItem("password");
+      window.location.href = "login.html";
     });
     userStatusDiv.appendChild(greetingMessage);
     userStatusDiv.appendChild(logoutButton);
   } else {
     var loginButton = document.createElement("a");
     loginButton.textContent = "Login";
-    loginButton.href = "index.html";
+    loginButton.href = "login.html";
   }
 });
--- login.js
+++ login.js
@@ -1,24 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   var loginForm = document.getElementById("login-form");
   var messageDiv = document.getElementById("message");
-
   loginForm.addEventListener("submit", function (e) {
     e.preventDefault();
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    var email =document.getElementById("email").value;
-
-
-    
+    var email = document.getElementById("email").value;
     if (username && password) {
       localStorage.setItem("isLoggedIn", "true");
       localStorage.setItem("username", username);
-      localStorage.setItem("password", password)
+      localStorage.setItem("password", password);
 
       messageDiv.innerHTML = "Login successful! Redirecting...";
       setTimeout(function () {
-        window.location.href = "homepage.html";
-      }, 2000); 
+        window.location.href = "index.html";
+      }, 2000);
     } else {
       messageDiv.innerHTML = "Login failed. Please check your credentials.";
     }
